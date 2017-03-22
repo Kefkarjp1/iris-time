afa129427edea607c6793c00e37a7ab3c3cef64b
Kefkarjp1 <indigo.poirier@pqatesting.ca>
2017-03-22 15:23:10 -0300

    Added dynamic iris url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,16 +5,16 @@ module.exports = {
             script: 'bin/run.js',
             env_production: {
                 NODE_ENV: 'production',
-                IRIS_URL: 'http://35.160.143.122:3000'
+                IRIS_URL: 'http://<IP_OF_IRIS_MAIN>:3000'
             }
         }
     ],
     deploy: {
         production: {
             user: 'node',
-            host: '35.167.25.80',
+            host: '<YOUR SERVER IP>',
             ref: 'origin/master',
-            repo: 'https://github.com/lynda-danielkhan/iris-time.git',
+            repo: 'https://github.com/<YOUR_REPO_URL>',
             path: '/srv/production',
             'post-deploy': 'cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production'
         }
